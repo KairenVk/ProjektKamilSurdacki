7bde3ea59028ae99a6072108f7d59239884ae478
KairenVk <kamilsurdacki01@gmail.com>
2023-05-06 11:21:05 +0200

    First commit

---
+++ src/main/java/com/tss/ProjektKamilSurdacki/ProjektKamilSurdackiApplication.java
@@ -0,0 +1,13 @@
+package com.tss.ProjektKamilSurdacki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKamilSurdackiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektKamilSurdackiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tss/ProjektKamilSurdacki/ProjektKamilSurdackiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.ProjektKamilSurdacki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKamilSurdackiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
